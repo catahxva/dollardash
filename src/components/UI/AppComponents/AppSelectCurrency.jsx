96293016876990f6b2431de11cea3f9a88c598f9
catahxva <havacatalin459@gmail.com>
2024-03-07 19:40:43 +0200

    small changes

--- src/components/UI/AppComponents/AppSelectCurrency.jsx
+++ src/components/UI/AppComponents/AppSelectCurrency.jsx
@@ -1,15 +1,50 @@
 import classes from "./AppSelectCurrency.module.css";
 
+import { useState, useEffect } from "react";
+
+import { useSelector } from "react-redux";
+
+import { EXCHANGE_KEY } from "../../../util/config";
+
 function AppSelectCurrency() {
+  // const []
+
+  const currentCurrency = useSelector((state) => state.general.currency);
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+
+  const changeHandler = async function (e) {
+    try {
+      const res = await fetch(
+        `http://api.exchangeratesapi.io/v1/convert?access_key=${EXCHANGE_KEY}&from=${currentCurrency}&to=${e.target.value}&amount=1`
+      );
+
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(currentCurrency);
+
   return (
     <form className={classes.select__form}>
       <label className={classes.select__form__span} htmlFor="select">
         Currency:
       </label>
-      <select className={classes.select__form__select} name="select">
-        <option>USD ($)</option>
-        <option>EUR (€)</option>
-        <option>YEN (¥)</option>
+      <select
+        onChange={changeHandler}
+        defaultValue={currentCurrency}
+        className={classes.select__form__select}
+        name="select"
+      >
+        <option value="USD">USD ($)</option>
+        <option value="EUR">EUR (€)</option>
+        <option value="YEN">YEN (¥)</option>
       </select>
     </form>
   );
---
+++ src/util/config.js
@@ -0,0 +1,1 @@
+export const EXCHANGE_KEY = "f2af70fb764ec266ad640af17b76dd19";
